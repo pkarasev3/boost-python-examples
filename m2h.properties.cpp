33fdd6ca720a85a1f1d40224ec8bec0e99a84f21
Troy Straszheim <straszheim@willowgarage.com>
2011-04-04 13:48:32 -0700

    bing

---
+++ m2h.properties.cpp
@@ -0,0 +1,31 @@
+#include <boost/python.hpp>
+#include <iostream>
+
+using namespace boost::python;
+
+struct MyClass
+{
+  void set_value(int j) { 
+    std::cout << "setting to " << j << "\n";
+    value = j; 
+  }
+  int get_value() { 
+    std::cout << "getting!\n";
+    return value; 
+  }
+
+  MyClass(int v) : value(v) { }
+  MyClass() : value(700) { }
+
+private:
+  int value;
+};
+
+BOOST_PYTHON_MODULE(m2h)
+{
+  class_<MyClass>("MyClass")
+    .def(init<int>())
+    .add_property("value", &MyClass::get_value, &MyClass::set_value)
+    .add_property("rovalue", &MyClass::get_value)
+    ;
+}
