80161eb0afbaee253783c16ea29e0f00830b96cf
Troy Straszheim <straszheim@willowgarage.com>
2011-04-05 11:11:12 -0700

    bing

---
+++ m2o.holder2.cpp
@@ -0,0 +1,49 @@
+#include <boost/python.hpp>
+#include <iostream>
+using namespace boost::python;
+
+struct T 
+{ 
+  T() { 
+    std::cout << __PRETTY_FUNCTION__ << "\n"; 
+  }
+  T(const T& rhs) { 
+    std::cout << __PRETTY_FUNCTION__ << "\n"; 
+  }
+  std::string foo() { return "T::foo() !!!"; }
+};
+
+std::string takes_tptr(boost::shared_ptr<T> tptr)
+{ 
+  std::cout << "shared_ptr to " << tptr.get() << " has refcount " << tptr.use_count() << "\n";
+  return tptr->foo();
+}
+
+std::string takes_trawptr(T* tptr)
+{ 
+  return tptr->foo();
+}
+
+std::string takes_tref(T& tref)
+{ 
+  return tref.foo();
+}
+
+boost::shared_ptr<T> make_t()
+{
+  static boost::shared_ptr<T> tptr(new T);
+  return tptr;
+}
+
+BOOST_PYTHON_MODULE(m2o)
+{
+  class_<T, boost::shared_ptr<T> >("T")
+    .def("foo", &T::foo)
+    ;
+
+  def("takes_trawptr", &takes_trawptr);
+  def("takes_tptr", &takes_tptr);
+  def("takes_tref", &takes_tref);
+  def("make_t", &make_t);
+}
+
